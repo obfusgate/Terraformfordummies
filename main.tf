65a63c26dcb8d8358843a9826a26da93775d415d
Obfusgate <christopher_snook@outlook.com>
2021-02-23 16:05:19 -0800

    Associate Subnet with route table

--- main.tf
+++ main.tf
@@ -58,3 +58,9 @@ resource "aws_route_table" "prod-route-table" {
   tags = {
     Name = "prod-subnet"
   }
+  # 5. Associate Subnet with route table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
