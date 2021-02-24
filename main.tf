3121c43203d1cb4a1d34ca7ae0075e0e87b4eeee
Obfusgate <christopher_snook@outlook.com>
2021-02-23 16:04:30 -0800

    Create a Subnet

--- main.tf
+++ main.tf
@@ -48,3 +48,13 @@ resource "aws_route_table" "prod-route-table" {
     Name = "prod"
   }
 }
+# 4. Create a Subnet
+
+ resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
