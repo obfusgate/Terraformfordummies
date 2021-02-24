2d2da5cdd4e5bf39575cbed98ef8212dae993df0
Obfusgate <christopher_snook@outlook.com>
2021-02-23 16:03:18 -0800

    Create custom route table

--- main.tf
+++ main.tf
@@ -29,3 +29,22 @@ resource "aws_internet_gateway" "gw" {
 
 
 }
+# 3. Create custom route table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
